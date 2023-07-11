94152b5bb8f5c9d2ac2aab2eb0598e5b0123eac5
lomayd <jinho10167@gmail.com>
2023-07-11 14:48:30 +0000

    feat: Project Initialize

---
+++ src/main/java/lomayd/DBMSLabReplication/api/DbmsLabReplicationApplication.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabReplication.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsLabReplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsLabReplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/DBMSLabReplication/DbmsLabReplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabReplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsLabReplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
